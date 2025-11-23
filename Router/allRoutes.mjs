5bcee2395e91a3c0e8367075e01489bc34ed76b8
Mamoudou18 <mamoudou.traore180422@gmail.com>
2025-11-23 12:07:49 +0100

    ajout page de connexion

--- Router/allRoutes.mjs
+++ Router/allRoutes.mjs
@@ -2,7 +2,10 @@ import Route from "./Route.mjs";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/signin", "Connxion", "/pages/signin.html"),
+];
+    
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Vite & Gourmand";
