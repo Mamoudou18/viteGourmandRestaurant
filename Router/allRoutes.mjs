b9a8f47c6eb989f5e1dfc8badf77efc8acc7ce76
Mamoudou18 <mamoudou.traore180422@gmail.com>
2025-11-23 18:00:01 +0100

    Ajout de l'option mot de passe oublié

--- Router/allRoutes.mjs
+++ Router/allRoutes.mjs
@@ -3,8 +3,10 @@ import Route from "./Route.mjs";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin", "Connxion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signin", "Connxion", "/pages/authent/signin.html"),
+    new Route("/signup", "Inscription", "/pages/authent/signup.html"),
+    new Route("/initPasseword", "Initialisation de mot de passe", "/pages/authent/initPasseword.html"),
+
 ];
     
 
